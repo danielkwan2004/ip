c8250ca04f60a159a59eb8d80ea0562936f76be1
danielkwan2004 <77553178+danielkwan2004@users.noreply.github.com>
2025-08-19 22:40:36 +0800

    Change name of default chatbot name, Duke, to ChatTPG. Remove all traces of Duke, and implement a skeletal version of the chatbot that simply greets the user and exits.

---
+++ src/main/java/ChatTPG.java
@@ -0,0 +1,8 @@
+public class ChatTPG {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ChatTPG");
+        System.out.println("What can I do for you?");
+        System.out.println("");
+        System.out.println("You shut me down... how sad. Maybe we will meet again...");
+    }
+}
