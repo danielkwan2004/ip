82cbf2d06528ed34332b72a3e973e0930eabd791
danielkwan2004 <e1468338@u.nus.edu>
2025-09-03 13:19:30 +0800

    Add Level-1
    Level-1 task echos commands entered by the user

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# ChatTPG User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/ChatTPG.java
+++ src/main/java/ChatTPG.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class ChatTPG {
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChatTPG");
         System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println("You shut me down... how sad. Maybe we will meet again...");
+        
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        System.out.println("\t" + userInput);
+        // System.out.println("");
+        // System.out.println("You shut me down... how sad. Maybe we will meet again...");
     }
 }
+
