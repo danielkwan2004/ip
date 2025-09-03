838d5629d9e1e51532868a84f70205375bb9560e
danielkwan2004 <77553178+danielkwan2004@users.noreply.github.com>
2025-09-03 18:10:34 +0800

    Add markTaskAsDone (Level-3 task, incomplete)

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,36 @@
-// Add the ability to store whatever text entered by the user and display them back to the user when requested.
 public class Task {
-    
 
-    String[] taskList = new String[100];
+    private Boolean isDone;
+    static int MAX_TASKS = 100;
+
+    String[] taskList = new String[MAX_TASKS];
     int taskCount = 0;
 
-    public void addTask(String description){
-        taskList[taskCount] = description;
+    public void addTask(String description) {
+        taskList[taskCount] = "[ ] " + description;
+        System.out.println("\tadded: " + taskList[taskCount]);
         taskCount++;
-        System.out.println("\tadded: " + description);
     }
 
-    public void listTask(){
+    public void listTask() {
+        System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < taskCount; i++) {
             System.out.println("\t" + (i + 1) + ". " + taskList[i]);
         }
     }    
+
+    public void markTaskAsDone(int taskNumber) {
+        if (taskNumber < 0 || taskNumber >= 100) {
+            System.out.println("You have gone out of bounds. This task number does not exist.");
+            return;
+        }
+        int taskIndex = taskNumber - 1;
+        taskList[taskCount]
+        System.out.println("Nice! I've marked task number %d as done. The following is the name of the task: ", taskNumber);
+        System.out.println("  " + )
+
+
+
+    }
+
 }
